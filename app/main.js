b63384954fcbcbee283e70948158554a9829d818
Stefan Dierauf <sdierauf@gmail.com>
2014-09-24 17:01:07 -0700

    nukage

--- app/main.js
+++ app/main.js
@@ -21,20 +21,6 @@ require([
     'staff/staffController',
     'studyabroad/studyAbroadController',
     'cloud/cloudController'
-//    'filters/formatters',
-//    'snapshotDiffs/snapshotDiffDetailController',
-//    'home/headerController',
-//    'home/homeController',
-//    'pages/pageController',
-//    'pages/pageBookmarkletController',
-//    'pages/pageDetailController',
-//    'compares/compareController',
-//    'compares/compareDetailController',
-//    'compares/compareResultDetailController',
-//    'snapshots/snapshotDetailController',
-//    'suites/suiteController',
-//    'suites/suiteDetailController',
-//    'suites/suiteRunController'
 ], function (
     angular,
     myCSEApp,
@@ -46,20 +32,6 @@ require([
     StaffController,
     StudyAbroadController,
     CloudController
-//    ScyllaFormatters,
-//    SnapshotDiffDetailController,
-//    HeaderController,
-//    HomeController,
-//    PageController,
-//    ReportBookmarkletController,
-//    ReportDetailController,
-//    ComparesController,
-//    CompareDetailController,
-//    CompareResultDetailController,
-//    SnapshotDetailController,
-//    SuiteController,
-//    SuiteDetailController,
-//    SuiteRunController
     ) {
 
     'use strict';
@@ -89,44 +61,6 @@ require([
             {templateUrl: 'app/staff/staff.html',
                 controller: 'StaffController'})
             .otherwise({redirectTo:"/home"});
-//            .when('/home',
-//            {templateUrl:'app/home/home.html',
-//                controller:"HomeController"})
-//            .when('/pages',
-//            {templateUrl:'app/pages/pages.html',
-//                controller:"PageController"})
-//            .when('/pages/bookmarklet',
-//            {templateUrl:'app/pages/pageBookmarklet.html',
-//                controller:"PageBookmarkletController"})
-//            .when('/pages/:id',
-//            {templateUrl:'app/pages/pageDetail.html',
-//                controller:"PageDetailController"})
-//            .when('/compares',
-//            {templateUrl:'app/compares/compares.html',
-//                controller:"CompareController"})
-//            .when('/compares/:id',
-//            {templateUrl:'app/compares/compareDetail.html',
-//                controller:"CompareDetailController"})
-//            .when('/compares/:compareId/results/:id',
-//            {templateUrl:'app/compares/compareResultDetail.html',
-//                controller:"CompareResultDetailController"})
-//            .when('/snapshotDiffs/:id',
-//            {templateUrl:'app/snapshotDiffs/snapshotDiffDetail.html',
-//                controller:"SnapshotDiffDetailController"})
-//            .when('/suites',
-//            {templateUrl:'app/suites/suites.html',
-//                controller:"SuiteController"})
-//            .when('/suites/:suiteId/suiteRuns/:suiteRunId',
-//            {templateUrl:'app/suites/suiteRun.html',
-//                controller:"SuiteRunController",
-//                reloadOnSearch:false})
-//            .when('/suites/:id',
-//            {templateUrl:'app/suites/suiteDetail.html',
-//                controller:"SuiteDetailController"})
-//            .when('/snapshots/:id',
-//            {templateUrl:'app/snapshots/snapshotDetail.html',
-//                controller:"SnapshotDetailController"})
-//            .otherwise({redirectTo:"/home"})
     }]);
 
 
