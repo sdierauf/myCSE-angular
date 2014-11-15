ff38d5693f40219d87483faea3ecb41ef87b5150
Stefan Dierauf <sdierauf@cs.washington.edu>
2014-11-14 16:51:00 -0800

    finished little mock api example with services

--- app/home/homeController.js
+++ app/home/homeController.js
@@ -1,13 +1,27 @@
 define([
     "myCSEApp",
+    "services/usersService",
     "home/header"
 ], function(
-    myCSEApp
+    myCSEApp,
+    theUsersService
     ){
     'use strict';
 
-    return myCSEApp.controller("HomeController", function($scope, $http, Header) {
+    return myCSEApp.controller("HomeController", function($scope, $http, Header, UsersService) {
         Header.setFirstLevelNavId('homeNav');
+
+        $scope.user = {};
+
+        $scope.loadUser = function loadUser(id) {
+            UsersService.getById(1232328)
+              .then(function (response) {
+                  console.log(response);
+                  $scope.user = response;
+              });
+        }
+
+        $scope.loadUser(10);
     });
 
 });
